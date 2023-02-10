479ab81bad15a25f08172b1058402b226fb54811
brodilin ivan <brodilinivan@yandex.ru>
2023-02-10 16:05:52 +0400

    1 задача решена

--- DZ/task1.py
+++ DZ/task1.py
@@ -4,3 +4,17 @@
 Об окончании ввода данных свидетельствует пустая строка.
 """
 
+with open("test_file.txt", "w", encoding='utf-8') as my_file:
+
+    def input_line():
+        line_str = input(f'Введите строку: ')
+        if not line_str:
+            return
+        else:
+            my_file.write(f'{line_str}\n')
+            input_line()
+
+    input_line()
+
+
+
